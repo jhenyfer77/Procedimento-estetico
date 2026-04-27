4168b316c7d4787a830890baca417977eccee2cf
jhenyferr <jhenyferr66@gmail.com>
2026-04-27 12:13:19 -0300

    adiciona seleção de procedimento com preço dinâmico

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function salvar() {
     alert("Dados salvo com sucesso!");
 }
+
+function mostrarPreco() {
+    let select = document.getElementById("procedimento");
+    let preco = select.value;
+
+    if (preco === "") {
+        document.getElementById("preco").innerText = "Preço: R$ 0";
+    } else {
+        document.getElementById("preco").innerText = "Preço: R$ " + preco;
+    }
+}
