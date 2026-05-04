36ba362081b68926c41f53bc1551bf4f87bc5091
jhenyferr <jhenyferr66@gmail.com>
2026-05-04 13:34:55 -0300

    sei la fiz um mpnte de alteração

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function mostrarPreco() {
         document.getElementById("preco").innerText = "Preço: R$ " + preco;
     }
 }
+function mostrarInfo(nome, descricao, preco) {
+    document.getElementById("info").innerText =
+        nome + " - " + descricao + " | Preço: R$ " + preco;
+}
+
+function abrir(img, nome, desc, preco) {
+    document.querySelector(".catalogo").style.display = "none";
+    document.getElementById("detalhe").style.display = "block";
+
+    document.getElementById("imgDetalhe").src = img;
+    document.getElementById("nomeDetalhe").innerText = nome;
+    document.getElementById("descDetalhe").innerText = desc;
+    document.getElementById("precoDetalhe").innerText = "R$ " + preco;
+}
+
+function fechar() {
+    document.querySelector(".catalogo").style.display = "flex";
+    document.getElementById("detalhe").style.display = "none";
+}
