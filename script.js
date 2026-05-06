e09a9af56ce6d0613f33262004ca5b12e31230fd
jhenyferr <jhenyferr66@gmail.com>
2026-05-06 13:43:55 -0300

    modificação de logotipo

--- script.js
+++ script.js
@@ -12,14 +12,16 @@ function mostrarPreco() {
         document.getElementById("preco").innerText = "Preço: R$ " + preco;
     }
 }
-function mostrarInfo(nome, descricao, preco) {
-    document.getElementById("info").innerText =
-        nome + " - " + descricao + " | Preço: R$ " + preco;
-}
 
 function abrir(img, nome, desc, preco) {
-    document.querySelector(".catalogo").style.display = "none";
-    document.getElementById("detalhe").style.display = "block";
+    // esconder tudo
+    document.getElementById("conteudo").style.display = "none";
+    document.getElementById("titulo").style.display = "none";
+    document.getElementById("menu").style.display = "none";
+
+    // mostrar detalhe centralizado
+    let detalhe = document.getElementById("detalhe");
+    detalhe.style.display = "flex";
 
     document.getElementById("imgDetalhe").src = img;
     document.getElementById("nomeDetalhe").innerText = nome;
@@ -28,6 +30,9 @@ function abrir(img, nome, desc, preco) {
 }
 
 function fechar() {
-    document.querySelector(".catalogo").style.display = "flex";
+    document.getElementById("conteudo").style.display = "block";
+    document.getElementById("titulo").style.display = "block";
+    document.getElementById("menu").style.display = "block";
+
     document.getElementById("detalhe").style.display = "none";
 }
